48deccd7c771ebf2d035a2609ea245d0d43a4cc9
Eunji-Elly-Lee <eunji.lee@edu.sait.ca>
2022-05-22 20:22:43 -0600

    Create gimbap.sql

---
+++ src/java/database/gimbap.sql
@@ -0,0 +1,9 @@
+/* ****************************************************************************
+** Author: Eunji Elly Lee
+** Created: May 22, 2022
+** Description: 
+***************************************************************************** */
+
+DROP SCHEMA IF EXISTS `gimbap`;
+CREATE SCHEMA IF NOT EXISTS `gimbap` DEFAULT CHARACTER SET latin1;
+USE `gimbap`;
