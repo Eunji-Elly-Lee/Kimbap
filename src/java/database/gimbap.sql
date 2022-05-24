c1aacede6d563b830a6a7711b6d6367a59deaa9e
Eunji-Elly-Lee <eunji.lee@edu.sait.ca>
2022-05-23 23:45:16 -0600

    Update gimbap.sql

--- src/java/database/gimbap.sql
+++ src/java/database/gimbap.sql
@@ -7,3 +7,14 @@
 DROP SCHEMA IF EXISTS `gimbap`;
 CREATE SCHEMA IF NOT EXISTS `gimbap` DEFAULT CHARACTER SET latin1;
 USE `gimbap`;
+
+-- -----------------------------------------------------
+-- Table `gimbap`.`user`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `gimbap`.`user` (
+  `user_id` INT(12) NOT NULL AUTO_INCREMENT,
+  `email` VARCHAR(120) NOT NULL,
+  `name` VARCHAR(120) NOT NULL,
+  `code` VARCHAR(10),
+  `salt` VARCHAR(50),
+  PRIMARY KEY (`user_id`));
