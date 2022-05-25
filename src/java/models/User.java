48b45856fa01341569d8494b5e718d99d6ba2aee
Eunji-Elly-Lee <eunji.lee@edu.sait.ca>
2022-05-25 07:05:49 -0600

    Update User.java

--- src/java/models/User.java
+++ src/java/models/User.java
@@ -1,24 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package models;
 
 import java.io.Serializable;
-import javax.persistence.Basic;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.NamedQueries;
-import javax.persistence.NamedQuery;
-import javax.persistence.Table;
+import javax.persistence.*;
 import javax.xml.bind.annotation.XmlRootElement;
 
-/**
- *
- * @author duckp
- */
 @Entity
 @Table(name = "user")
 @XmlRootElement
@@ -94,5 +79,4 @@ public class User implements Serializable {
     public String toString() {
         return "models.User[ email=" + email + " ]";
     }
-    
 }
