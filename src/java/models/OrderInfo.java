54c3aa2145b017d0e272941e2adabc4e8da85b05
Eunji-Elly-Lee <eunji.lee@edu.sait.ca>
2022-05-25 07:04:54 -0600

    Update OrderInfo.java

--- src/java/models/OrderInfo.java
+++ src/java/models/OrderInfo.java
@@ -1,29 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package models;
 
 import java.io.Serializable;
-import javax.persistence.Basic;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.NamedQueries;
-import javax.persistence.NamedQuery;
-import javax.persistence.Table;
+import javax.persistence.*;
 import javax.xml.bind.annotation.XmlRootElement;
 
-/**
- *
- * @author duckp
- */
 @Entity
 @Table(name = "order")
 @XmlRootElement
@@ -106,5 +86,4 @@ public class OrderInfo implements Serializable {
     public String toString() {
         return "models.OrderInfo[ orderId=" + orderId + " ]";
     }
-    
 }
