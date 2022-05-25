71e7e54194aa919b3023bbb7799878b791025b56
Eunji-Elly-Lee <eunji.lee@edu.sait.ca>
2022-05-25 07:03:58 -0600

    Update Location.java

--- src/java/models/Location.java
+++ src/java/models/Location.java
@@ -1,31 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package models;
 
 import java.io.Serializable;
 import java.util.List;
-import javax.persistence.Basic;
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.NamedQueries;
-import javax.persistence.NamedQuery;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
-import javax.xml.bind.annotation.XmlRootElement;
-import javax.xml.bind.annotation.XmlTransient;
+import javax.persistence.*;
+import javax.xml.bind.annotation.*;
 
-/**
- *
- * @author duckp
- */
 @Entity
 @Table(name = "location")
 @XmlRootElement
@@ -108,5 +87,4 @@ public class Location implements Serializable {
     public String toString() {
         return "models.Location[ locationId=" + locationId + " ]";
     }
-    
 }
