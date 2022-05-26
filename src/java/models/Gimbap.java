7bdc7d37431bda3062774179d11eb5628903c360
Eunji-Elly-Lee <eunji.lee@edu.sait.ca>
2022-05-26 00:07:16 -0600

    Update Gimbap.java

--- src/java/models/Gimbap.java
+++ src/java/models/Gimbap.java
@@ -1,31 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package models;
 
 import java.io.Serializable;
 import java.util.List;
-import javax.persistence.Basic;
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.NamedQueries;
-import javax.persistence.NamedQuery;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
-import javax.xml.bind.annotation.XmlRootElement;
-import javax.xml.bind.annotation.XmlTransient;
+import javax.persistence.*;
+import javax.xml.bind.annotation.*;
 
-/**
- *
- * @author duckp
- */
 @Entity
 @Table(name = "gimbap")
 @XmlRootElement
@@ -35,7 +14,6 @@ import javax.xml.bind.annotation.XmlTransient;
     , @NamedQuery(name = "Gimbap.findByGimbapName", query = "SELECT g FROM Gimbap g WHERE g.gimbapName = :gimbapName")
     , @NamedQuery(name = "Gimbap.findByPrice", query = "SELECT g FROM Gimbap g WHERE g.price = :price")})
 public class Gimbap implements Serializable {
-
     private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -121,5 +99,4 @@ public class Gimbap implements Serializable {
     public String toString() {
         return "models.Gimbap[ gimbapId=" + gimbapId + " ]";
     }
-    
 }
