1c41648591259db3f817dcd6889be1034815459e
Eunji-Elly-Lee <eunji.lee@edu.sait.ca>
2022-06-28 22:31:43 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import Header from "./components/Header"
+import Home from "./routes/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 function App() {
   return (
-    <div className="App">
-      
+    <div className="app-wrapper">
+      <Router>
+        <Header />
+        <Routes>
+          <Route path={`${process.env.PUBLIC_URL}/`} element={<Home />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
