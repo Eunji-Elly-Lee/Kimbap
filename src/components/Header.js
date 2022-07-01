386805c27b3debccbd318175eaa99e2ec8fc4099
Eunji-Elly-Lee <eunji.lee@edu.sait.ca>
2022-06-30 23:59:46 -0600

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,18 +6,22 @@ function Header() {
   return (
     <div>
       <Container>
-        <Row xs="auto" className="align-items-center">
-          <Col className="menu">
+        <Row xs="auto" className="mt-1 align-items-center">
+          <Col className="header-menu p-0">
             <Link to="/">
               <img className="header-logo" src="./assets/logo_black.gif" alt="logo" />
             </Link>
           </Col>
-          <Col className="menu">
-            <Link className="signin" to="/signin">Sign In</Link>
+          <Col className="header-menu p-0">
+            <Link to="/signin" className="signin position-relative text-dark">
+              Sign In
+            </Link>
           </Col>
-          <Col className="menu">
+          <Col className="header-menu">
             <Link to="/signup">
-              <Button variant="outline-dark" className="signup">Sign Up</Button>
+              <Button variant="outline-dark" className="signup">
+                Sign Up
+              </Button>
             </Link>
           </Col>
         </Row>
