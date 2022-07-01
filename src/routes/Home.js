a0a506508584f61b64ea8b43fefb9914b7beb2ce
Eunji-Elly-Lee <eunji.lee@edu.sait.ca>
2022-06-30 23:33:38 -0600

    Update Home.js

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,9 +1,28 @@
+import { Container, Row, Col } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import './Home.css';
 
 function Home() {
   return (
-    <div>
-      
+    <div className="home">
+      <Container>
+        <Row className="mt-4 justify-content-evenly">
+          <Col xs="5" sm="4" md="3"
+            className="main-menu m-5 m-sm-2 mt-sm-5 position-relative">
+            <Link to="/menu"
+              className="w-100 h-100 d-flex align-items-center text-center">
+              <span className="w-100 text-dark fs-2">Menu</span>
+            </Link>
+          </Col>
+          <Col xs="5" sm="4" md="3"
+            className="main-menu m-5 m-sm-2 mt-sm-5 position-relative">
+            <Link to="/orders"
+              className="w-100 h-100 d-flex align-items-center text-center">
+              <span className="w-100 text-dark fs-2">Orders</span>
+            </Link>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
