f9529d47254f105d0f1a9031c5cd3a911d636102
Eunji-Elly-Lee <eunji.lee@edu.sait.ca>
2022-07-03 01:02:18 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Header from "./components/Header"
 import Home from "./routes/Home";
 import SignIn from "./routes/SignIn";
+import SignUp from "./routes/SignUp";
 import Footer from "./components/Footer";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path={`${process.env.PUBLIC_URL}/`} element={<Home />} />
           <Route basename={process.env.PUBLIC_URL} path="/signin" element={<SignIn />} />
+          <Route basename={process.env.PUBLIC_URL} path="/signup" element={<SignUp />} />
         </Routes>
         <Footer />
       </Router>
