e95378f260357b3d28fdd95b14f2e9140b1bd309
Eunji-Elly-Lee <eunji.lee@edu.sait.ca>
2022-07-03 01:00:01 -0600

    Create SignUp.js

---
+++ src/routes/SignUp.js
@@ -0,0 +1,39 @@
+import { Button, Col, Form, Row } from 'react-bootstrap';
+import { IoPersonAddSharp } from "react-icons/io5";
+import './SignUp.css';
+import { useState } from "react";
+
+function SignUp() {
+  const [email, setEmail] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (email === "") {
+      return;
+    }
+    setEmail("");
+  }
+
+  return (
+    <div className="signUp">
+      <Form onSubmit={handleSubmit}>
+        <Form.Group as={Row} className="justify-content-center">
+          <Form.Label column xs="1" className="signUp-icon text-center">
+            <IoPersonAddSharp />
+          </Form.Label>
+          <Col xs="10" className="p-0 me-2">
+            <Form.Control type="email" placeholder="Email"
+              className="border-0 border-bottom border-dark rounded-0"
+              value={email} onChange={(e) => setEmail(e.target.value)} />
+          </Col>
+        </Form.Group>
+        <Button variant="dark" type="submit"
+          className="signUp-button mt-5 d-block rounded-0">
+          Sign Up
+        </Button>
+      </Form>
+    </div>
+  );
+}
+
+export default SignUp;
