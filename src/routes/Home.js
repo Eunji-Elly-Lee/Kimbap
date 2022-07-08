266bcb937b8f487052c2ae4c13b5689c840d98e8
Eunji-Elly-Lee <eunji.lee@edu.sait.ca>
2022-07-07 22:15:22 -0600

    Update Home.js

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -11,14 +11,14 @@ function Home() {
             className="main-menu m-5 m-sm-2 mt-sm-5 position-relative">
             <Link to="/menu"
               className="w-100 h-100 d-flex align-items-center text-center">
-              <span className="w-100 text-dark fs-2">Menu</span>
+              <span className="w-100 text-white fs-2">Menu</span>
             </Link>
           </Col>
           <Col xs="5" sm="4" md="3"
             className="main-menu m-5 m-sm-2 mt-sm-5 position-relative">
             <Link to="/orders"
               className="w-100 h-100 d-flex align-items-center text-center">
-              <span className="w-100 text-dark fs-2">Orders</span>
+              <span className="w-100 text-white fs-2">Orders</span>
             </Link>
           </Col>
         </Row>
