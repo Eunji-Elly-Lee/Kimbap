e354315673614b07cc9530cf39d7a836d4aa6106
Eunji-Elly-Lee <eunji.lee@edu.sait.ca>
2022-07-09 18:54:06 -0600

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 import { Button, Container, Row, Col } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
-import './Header.css';
+import 'components/Header.css';
 
 function Header() {
   return (
