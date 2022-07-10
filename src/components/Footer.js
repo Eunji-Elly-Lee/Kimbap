11c9a396c34f524000126f1a2ef556ea8a2d77e5
Eunji-Elly-Lee <eunji.lee@edu.sait.ca>
2022-07-09 18:53:13 -0600

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,6 @@
 import { Container, Row, Col } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
-import './Footer.css';
+import 'components/Footer.css';
 
 function Footer() {
   return (
