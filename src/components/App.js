ac7a72c658243b2e8a0f715f8e6aaa225f8ddadc
Eunji-Elly-Lee <eunji.lee@edu.sait.ca>
2022-07-12 23:37:52 -0600

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,10 @@
-import Header from "components/Header"
-import Home from "routes/Home";
-import SignIn from "routes/SignIn";
-import SignUp from "routes/SignUp";
-import Menu from "routes/Menu";
-import Footer from "components/Footer";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from 'components/Header';
+import Home from 'routes/Home';
+import SignIn from 'routes/SignIn';
+import SignUp from 'routes/SignUp';
+import Menu from 'routes/Menu';
+import Footer from 'components/Footer';
 
 function App() {
   return (
