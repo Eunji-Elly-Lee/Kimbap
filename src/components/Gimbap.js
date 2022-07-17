90d1beab5d040b84d7caabfdc7c573f901544ea4
Eunji-Elly-Lee <eunji.lee@edu.sait.ca>
2022-07-16 20:48:36 -0600

    Update Gimbap.js

--- src/components/Gimbap.js
+++ src/components/Gimbap.js
@@ -1,7 +1,38 @@
+import { Form } from 'react-bootstrap';
+import 'components/Gimbap.css';
+
 function Gimbap() {
   return (
-    <div>
-      
+    <div className="mb-4">
+      <h4>Gimbaps</h4>
+      <Form.Group className="mt-3">
+        <div className="d-flex">
+          <Form.Check type="checkbox" id="gimbap01" />
+          <Form.Label htmlFor="gimbap01" className="ms-2">
+            Gimbpa01
+          </Form.Label>
+          <Form.Label className="mx-2">
+            $ 5.00
+          </Form.Label>
+          <Form.Control type="number" className="nums" />
+        </div>
+        <img src="./logo192.png" alt="gimbap01" />
+        <p>Ingredient01, Ingredient02, Ingredient03, Ingredient04</p>
+      </Form.Group>
+      <Form.Group className="mt-3">
+        <div className="d-flex">
+          <Form.Check type="checkbox" id="gimbap02" />
+          <Form.Label htmlFor="gimbap02" className="ms-2">
+            Gimbpa02
+          </Form.Label>
+          <Form.Label className="mx-2">
+            $ 5.00
+          </Form.Label>
+          <Form.Control type="number" className="nums" />
+        </div>
+        <img src="./logo192.png" alt="gimbap02" />
+        <p>Ingredient01, Ingredient02, Ingredient03, Ingredient04</p>
+      </Form.Group>
     </div>
   );
 }
