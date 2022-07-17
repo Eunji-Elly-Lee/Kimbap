71f0e4f0ce7bf3381972b75bb8e334b0babf32d2
Eunji-Elly-Lee <eunji.lee@edu.sait.ca>
2022-07-16 20:52:43 -0600

    Update Location.js

--- src/components/Location.js
+++ src/components/Location.js
@@ -7,19 +7,19 @@ function Location() {
       <h4>Pick Up Location</h4>
       <Form.Group className="d-flex mt-3">
         <Form.Check type="radio" id="pickUp-location01" />
-        <Form.Label htmlFor="pickUp-location01" className="m-0 ms-2">
+        <Form.Label htmlFor="pickUp-location01" className="ms-2">
           Mon. 8:00 A.M. AAA Station
         </Form.Label>
       </Form.Group>
-      <Form.Group className="d-flex mt-3">
+      <Form.Group className="d-flex">
         <Form.Check type="radio" id="pickUp-location02" />
-        <Form.Label htmlFor="pickUp-location02" className="m-0 ms-2">
+        <Form.Label htmlFor="pickUp-location02" className="ms-2">
           Mon. 8:00 A.M. AAA Station
         </Form.Label>
       </Form.Group>
-      <Form.Group className="d-flex mt-3">
+      <Form.Group className="d-flex">
         <Form.Check type="radio" id="pickUp-location03" />
-        <Form.Label htmlFor="pickUp-location03" className="m-0 ms-2">
+        <Form.Label htmlFor="pickUp-location03" className="ms-2">
           Mon. 8:00 A.M. AAA Station
         </Form.Label>
       </Form.Group>
