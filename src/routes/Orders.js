aec05d4056cc666070b0c519925b4710fc3a2c4e
Eunji-Elly-Lee <eunji.lee@edu.sait.ca>
2022-07-17 21:52:40 -0600

    Update Orders.js

--- src/routes/Orders.js
+++ src/routes/Orders.js
@@ -3,14 +3,24 @@ import 'routes/Orders.css';
 function Orders() {
   return (
     <div className="w-75 mx-auto my-5">
-      <h4>Order01</h4>
-      <p>Gimbpa01 (2EA) AAA Station</p>
-      <h4>Order02</h4>
-      <p>Gimbpa01 (2EA) AAA Station</p>
-      <h4>Order03</h4>
-      <p>Gimbpa01 (2EA) AAA Station</p>
-      <h4>Order04</h4>
-      <p>Gimbpa01 (2EA) AAA Station</p>
+      <div>
+        <h4>2022-07-17</h4>
+        <p>Elly - Gimbpa01 (2EA) AAA Station</p>
+        <p>Mocha - Gimbpa01 (2EA) AAA Station</p>
+        <h4>2022-07-18</h4>
+        <p>Elly - Gimbpa01 (2EA) AAA Station</p>
+        <p>Mocha - Gimbpa01 (2EA) AAA Station</p>
+      </div>
+      <div>
+        <h4>Order01</h4>
+        <p>Gimbpa01 (2EA) AAA Station</p>
+        <h4>Order02</h4>
+        <p>Gimbpa01 (2EA) AAA Station</p>
+        <h4>Order03</h4>
+        <p>Gimbpa01 (2EA) AAA Station</p>
+        <h4>Order04</h4>
+        <p>Gimbpa01 (2EA) AAA Station</p>
+      </div>
     </div>
   );
 }
