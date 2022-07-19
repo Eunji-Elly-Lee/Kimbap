89410bcd5434c6ceb22689fbbe829a8682eca34d
Eunji-Elly-Lee <eunji.lee@edu.sait.ca>
2022-07-19 11:30:51 -0600

    Update SignIn.js

--- src/routes/Signin.js
+++ src/routes/Signin.js
@@ -1,22 +1,58 @@
+import { useNavigate } from 'react-router-dom';
 import { useState } from 'react';
-import { Form, Row, Col, Button } from 'react-bootstrap';
+import { authService, firebaseInstance } from 'fbase';
+import { Form, Row, Col, Button, Spinner } from 'react-bootstrap';
 import { IoPersonSharp } from 'react-icons/io5';
 import 'routes/SignIn.css';
 
 function SignIn() {
+  const navigate = useNavigate();
   const [email, setEmail] = useState("");
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  const [sendingEmail, setSendingEmail] = useState(false);
+  const [message, setMessage] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const onSubmit = async (event) => {
+    event.preventDefault();
     if (email === "") {
       return;
     }
-    setEmail("");
-  }
+    try {
+      setSendingEmail(true);
+      const actionCodeSettings = {
+        url: "http://localhost:3000",
+        handleCodeInApp: true
+      };
+      await authService.sendSignInLinkToEmail(email, actionCodeSettings)
+        .then(() => {
+          window.localStorage.setItem("emailForSignIn", email);
+          setEmail("");
+          setSendingEmail(false);
+          setMessage("An email has been sent! Please check your email!");
+        });
+    } catch (error) {
+      if (error.code === "auth/invalid-email") {
+        setMessage("The email address is not valid!");
+      }
+    }
+  };
+  const onClick = async () => {
+    try {
+      const provider = new firebaseInstance.auth.GoogleAuthProvider();
+      await authService.signInWithPopup(provider)
+        .then(() => {
+          navigate("/");
+        });
+    } catch (error) {
+      if (error.code === "auth/popup-blocked") {
+        setErrorMessage("The popup has been blocked by the browser!");
+      }
+    }
+  };
 
   return (
     <div className="signIn">
-      <Form onSubmit={handleSubmit}>
+      <div className="email-message mb-3 text-center">{message}</div>
+      <Form onSubmit={onSubmit}>
         <Form.Group as={Row} className="justify-content-center">
           <Form.Label column xs="1" className="signIn-icon text-center">
             <IoPersonSharp />
@@ -24,13 +60,26 @@ function SignIn() {
           <Col xs="10" className="p-0 me-2">
             <Form.Control type="email" placeholder="Email"
               className="border-0 border-bottom border-dark"
-              value={email} onChange={(e) => setEmail(e.target.value)} />
+              value={email} onChange={(event) => setEmail(event.target.value)} />
           </Col>
         </Form.Group>
-        <Button type="submit" className="signIn-button mt-5 d-block">
-          Sign In
+        <Button type="submit" variant="dark" disabled={sendingEmail}
+          className="signIn-button mt-5 d-block">
+          {sendingEmail ?
+            <>
+            <Spinner as="span" animation="border" size="sm"
+              role="status" aria-hidden="true" />
+            <span className="visually-hidden">Sending...</span>
+            </> :
+            "Sign In"
+          }
         </Button>
       </Form>
+      <Button type="submit" onClick={onClick}
+        className="signUp-button mt-5 d-block">
+        Continue with Google
+      </Button>
+      <div className="popup-error text-center">{errorMessage}</div>
     </div>
   );
 }
