6b23901588453cfcdceafcc7cac137a621a7a5ad
Eunji-Elly-Lee <eunji.lee@edu.sait.ca>
2022-07-21 00:31:07 -0600

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,32 @@
+import { useState, useEffect } from 'react';
+import { authService } from 'fbase';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from 'components/Header';
 import Home from 'routes/Home';
 import SignIn from 'routes/SignIn';
-import SignUp from 'routes/SignUp';
 import Menu from 'routes/Menu';
 import Orders from 'routes/Orders';
 import Footer from 'components/Footer';
 
 function App() {
+  const [userObj, setUserObj] = useState(null);
+  useEffect(() => {
+    authService.onAuthStateChanged((user) => {
+      if (user) {
+        setUserObj(user);
+      }
+    });
+  }, []);
+
   return (
     <div>
       <Router>
-        <Header />
+        <Header user={userObj} />
         <Routes>
           <Route path={`${process.env.PUBLIC_URL}/`} element={<Home />} />
-          <Route basename={process.env.PUBLIC_URL} path="/signin" element={<SignIn />} />
-          <Route basename={process.env.PUBLIC_URL} path="/signup" element={<SignUp />} />
-          <Route basename={process.env.PUBLIC_URL} path="/menu" element={<Menu />} />
-          <Route basename={process.env.PUBLIC_URL} path="/orders" element={<Orders />} />
+          <Route basename={process.env.PUBLIC_URL} path="/signin" element={<SignIn user={userObj} />} />
+          <Route basename={process.env.PUBLIC_URL} path="/menu" element={<Menu user={userObj} />} />
+          <Route basename={process.env.PUBLIC_URL} path="/orders" element={<Orders user={userObj} />} />
         </Routes>
         <Footer />
       </Router>
