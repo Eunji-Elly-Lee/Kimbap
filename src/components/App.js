ae95ba33c5ef2394cf1b87fd08278454d7f996ea
Eunji-Elly-Lee <eunji.lee@edu.sait.ca>
2022-07-21 21:06:01 -0600

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import { authService } from 'fbase';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from 'components/Header';
 import Home from 'routes/Home';
+import Auth from 'routes/Auth';
 import SignIn from 'routes/SignIn';
 import Menu from 'routes/Menu';
 import Orders from 'routes/Orders';
@@ -24,6 +25,7 @@ function App() {
         <Header user={userObj} />
         <Routes>
           <Route path={`${process.env.PUBLIC_URL}/`} element={<Home />} />
+          <Route basename={process.env.PUBLIC_URL} path="/auth" element={<Auth />} />
           <Route basename={process.env.PUBLIC_URL} path="/signin" element={<SignIn user={userObj} />} />
           <Route basename={process.env.PUBLIC_URL} path="/menu" element={<Menu user={userObj} />} />
           <Route basename={process.env.PUBLIC_URL} path="/orders" element={<Orders user={userObj} />} />
