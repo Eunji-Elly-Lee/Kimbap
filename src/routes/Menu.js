b1df864ef0990a800ca4e1162a200749381e2bd0
Eunji-Elly-Lee <eunji.lee@edu.sait.ca>
2022-07-24 19:30:20 -0600

    Update Menu.js

--- src/routes/Menu.js
+++ src/routes/Menu.js
@@ -1,22 +1,71 @@
-import Gimbap from 'components/Gimbap';
-import Location from 'components/Location';
-import Upload from 'components/Upload';
-import { Form, Button } from 'react-bootstrap';
+import { useState, useEffect } from 'react';
+import { dbService, authService } from 'fbase';
+import PropTypes from 'prop-types';
+import UploadGimbap from 'components/UploadGimbap';
+import UploadLocation from 'components/UploadLocation';
+import ManageGimbap from 'components/ManageGimbap';
+import ManageLocation from 'components/ManageLocation';
 import 'routes/Menu.css';
 
-function Menu() {
+function Menu({ user }) {
+  const [gimbaps, setGimbaps] = useState([]);
+  const [locations, setLocations] = useState([]);
+  useEffect(() => {
+    const unsubscribeGimbap = dbService.collection("gimbaps")
+      .orderBy("price")
+      .onSnapshot(snapshot => {
+        const gimbapArray = snapshot.docs.map(doc => ({
+          id: doc.id,
+          ...doc.data()
+      }));
+      setGimbaps(gimbapArray);
+    });
+    const unsubscribeLocation = dbService.collection("locations")
+      .onSnapshot(snapshot => {
+        const locationArray = snapshot.docs.map(doc => ({
+          id: doc.id,
+          ...doc.data()
+      }));
+      setLocations(locationArray);
+    });
+    authService.onAuthStateChanged((user) => {
+      if (user == null) {
+        unsubscribeGimbap();
+        unsubscribeLocation();
+      }
+    });
+  }, []);
+
   return (
     <div className="menu mx-auto mt-4">
-      <Upload />
-      <Form className="mt-4">
-        <Gimbap />
-        <Location />
-        <Button type="submit" className="w-100 mt-3">
-          Order
-        </Button>
-      </Form>
+      {(user && user.email === "lizyduck@gmail.com") ? (
+        <>
+        <UploadGimbap />
+        <UploadLocation />
+        <div className="mt-4">
+          <h4>Gimbaps</h4>
+          {gimbaps.map(gimbap => (
+            <ManageGimbap key={gimbap.id} gimbap={gimbap} />
+          ))}
+          <h4>Locations</h4>
+          {locations.map(location => (
+            <ManageLocation key={location.id} location={location} />
+          ))}
+        </div>
+        </>
+      ) : (
+        (user ? (
+          <></>
+        ) : (
+          <></>
+        ))
+      )}
     </div>
   );
 }
+
+Menu.propTypes = {
+  user: PropTypes.object
+};
 
 export default Menu;
