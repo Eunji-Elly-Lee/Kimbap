7053bb05b53977131fce877bfe92a56dafa730f5
Eunji-Elly-Lee <eunji.lee@edu.sait.ca>
2022-07-27 22:53:53 -0600

    Update OrderLocationForm.js

--- src/components/OrderLocationForm.js
+++ src/components/OrderLocationForm.js
@@ -1,11 +1,23 @@
+import { useStateValue } from 'StateProvider';
 import { Form } from 'react-bootstrap';
 import PropTypes from 'prop-types';
 
 function OrderLocationForm({ location }) {
+  const [ , dispatch] = useStateValue();
+  const onChange = (event) => {
+    if (event.target.checked) {
+      dispatch({
+        type: "SET_LOCATION",
+        location: location.location
+      });
+    };
+  }; 
+
   return (
     <div>
       <Form.Group className="d-flex mt-3">
-        <Form.Check type="radio" id={location.location} name="location" />
+        <Form.Check type="radio" id={location.location}
+          name="location" onChange={onChange} />
         <Form.Label htmlFor={location.location} className="ms-2">
           {location.location}
         </Form.Label>
