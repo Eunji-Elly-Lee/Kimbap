811ebc46224b9199c648c7e122362312b3a19e3c
Eunji-Elly-Lee <eunji.lee@edu.sait.ca>
2022-07-27 19:05:40 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { StateProvider } from 'StateProvider';
+import reducer, { initialState } from 'reducer';
 import App from 'components/App';
 import 'index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <StateProvider initialState={initialState} reducer={reducer}>
   <App />
+  </StateProvider>
 );
