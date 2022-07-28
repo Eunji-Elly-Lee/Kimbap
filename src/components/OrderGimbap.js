993693c3efc652f8314dc9036b433538d09b3c7d
Eunji-Elly-Lee <eunji.lee@edu.sait.ca>
2022-07-27 23:24:34 -0600

    Update OrderGimbap.js

--- src/components/OrderGimbap.js
+++ src/components/OrderGimbap.js
@@ -1,11 +1,24 @@
+import { useNavigate } from 'react-router-dom';
+import { useStateValue } from 'StateProvider';
+import { useState } from 'react';
 import { Form, Button } from 'react-bootstrap';
 import PropTypes from 'prop-types';
 import OrderGimbapForm from './OrderGimbapForm';
 import OrderLocationForm from './OrderLocationForm';
 
 function OrderGimbap({ gimbaps, locations }) {
-  const onSubmit = async (event) => {
-    event.preventDefault();    
+  const navigate = useNavigate();
+  const [state, ] = useStateValue();
+  const [message, setMessage] = useState("");
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (state.basket.length === 0) {
+      setMessage("Please select at least one gimbap!");
+    } else if (state.location === "") {
+      setMessage("Please select the pick up location!");
+    } else {
+      navigate("/order");
+    }
   };
 
   return (
@@ -19,6 +32,7 @@ function OrderGimbap({ gimbaps, locations }) {
         {locations.map(location => (
           <OrderLocationForm key={location.id} location={location} />
         ))}
+        <div className="mt-3 text-center">{message}</div>
         <Button type="submit" className="w-100 mt-3">
           Order
         </Button>
