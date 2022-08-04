155e4f72658064174956229f41c4ed7e504c9acd
Eunji-Elly-Lee <eunji.lee@edu.sait.ca>
2022-08-04 00:04:37 -0600

    Update index.js in functions

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,21 @@
-const functions = require("firebase-functions");
-
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const functions = require('firebase-functions');
+const express = require('express');
+const cors = require('cors');
+const stripe = require('stripe')(process.env.REACT_APP_STRIPE);
+const app = express();
+app.use(cors({
+  origin: true
+}));
+app.use(express.json());
+app.get('/', (request, response) => response.status(200).send('Server Response Success'));
+app.post('/payment/create', async (request, response) => {
+  const total = request.query.total;
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: total,
+    currency: 'CAD'
+  });
+  response.status(201).send({
+    clientSecret: paymentIntent.client_secret
+  })
+});
+exports.api = functions.https.onRequest(app);
