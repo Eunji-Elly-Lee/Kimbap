036a2f6c1c1ce57b82c8343ada9650c85ce0ef1b
Eunji-Elly-Lee <eunji.lee@edu.sait.ca>
2022-08-14 14:51:24 -0600

    Update index.js in functions

--- functions/index.js
+++ functions/index.js
@@ -1,13 +1,15 @@
 const functions = require('firebase-functions');
 const express = require('express');
 const cors = require('cors');
-const stripe = require('stripe')(process.env.REACT_APP_STRIPE);
+const stripe = require('stripe')(process.env.REACT_APP_STRIPE_SECRET_KEY);
 const app = express();
 app.use(cors({
   origin: true
 }));
 app.use(express.json());
-app.get('/', (request, response) => response.status(200).send('Server Response Success'));
+app.get('/', (request, response) => {
+  response.status(200).send('Server Response Success');
+});
 app.post('/payment/create', async (request, response) => {
   const total = request.query.total;
   const paymentIntent = await stripe.paymentIntents.create({
