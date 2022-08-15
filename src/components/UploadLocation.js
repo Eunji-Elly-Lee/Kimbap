5e543363087c8c11fd74f8ca2d13cea444a210b3
Eunji-Elly-Lee <eunji.lee@edu.sait.ca>
2022-08-15 00:48:43 -0600

    Update UploadLocation.js

--- src/components/UploadLocation.js
+++ src/components/UploadLocation.js
@@ -5,28 +5,36 @@ import { Form, Row, Col, Button, Spinner } from 'react-bootstrap';
 function UploadLocation() {
   const [location, setLocation] = useState("");
   const [uploading, setUploading] = useState(false);
+  const [message, setMessage] = useState("");
   const onSubmit = async (event) => {
     event.preventDefault();
     if (location === "") {
       return;
     }
-    setUploading(true);
-    await dbService.collection("locations").add({
-      location: location
-    }).then(() => {
-      setLocation("");
-      setUploading(false);
-    });    
+    const regExp = /^\d{4}\.\d{2}\.\d{2}\s[\w.:-\s]+$/;
+    if(regExp.test(location)) {
+      setUploading(true);
+      await dbService.collection("locations").add({
+        location: location
+      }).then(() => {
+        setLocation("");
+        setUploading(false);
+        setMessage("");
+      });  
+    } else {
+      setMessage("Please enter according to pattern!");
+    }
   };
 
   return (
     <div>
       <Form className="mt-4" onSubmit={onSubmit}>
         <h4>Upload Location</h4>
+        <div className="mb-3 text-center">{message}</div>
         <Form.Group as={Row} className="mt-3">
           <Form.Label column sm="2">Location:</Form.Label>
           <Col>
-            <Form.Control type="text" value={location}
+            <Form.Control type="text" value={location} placeholder="2022.01.01 Day Time Location"
               onChange={(event) => setLocation(event.target.value)} />
           </Col>
         </Form.Group>
