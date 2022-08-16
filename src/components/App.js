4001a73ae5f8d43ea23f09dae16df9415ce62d57
Eunji-Elly-Lee <eunji.lee@edu.sait.ca>
2022-08-16 15:50:32 -0600

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import { authService } from 'fbase';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import ScrollToTop from 'ScrollToTop';
 import Header from 'components/Header';
 import Home from 'routes/Home';
 import Auth from 'routes/Auth';
@@ -9,6 +10,7 @@ import Menu from 'routes/Menu';
 import Order from 'routes/Order';
 import Orders from 'routes/Orders';
 import Footer from 'components/Footer';
+import 'components/App.css';
 
 function App() {
   const [userObj, setUserObj] = useState(null);
@@ -24,9 +26,13 @@ function App() {
   }, []);
 
   return (
-    <div>
+    <div className="position-relative">
       <Router>
+        <ScrollToTop />
         <Header user={userObj} logOut={logOut} />
+        <div className="background-wrapper position-absolute w-50 d-none d-lg-block">
+          <img className="opacity-75 w-100" src="./assets/gimbap_bg.jpg" alt="background gimbap" />
+        </div>
         <Routes>
           <Route path={`${process.env.PUBLIC_URL}/`} element={<Home />} />
           <Route basename={process.env.PUBLIC_URL} path="/auth" element={<Auth />} />
