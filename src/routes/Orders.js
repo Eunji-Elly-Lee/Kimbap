9e6b1713d2461e1f08d6495eb4ed626d8449bf8d
Eunji-Elly-Lee <eunji.lee@edu.sait.ca>
2022-08-21 22:40:05 -0600

    Update Orders.js

--- src/routes/Orders.js
+++ src/routes/Orders.js
@@ -1,28 +1,44 @@
+import { useState, useEffect } from 'react';
+import { dbService } from 'fbase';
+import PropTypes from 'prop-types';
 import 'routes/Orders.css';
 
-function Orders() {
+function Orders({ user }) {
+  const [orders, setOrders] = useState([]);
+  useEffect(() => {
+    if (user) {
+      if (user.email === "lizyduck@gmail.com") {
+        dbService.collection("orders").orderBy("location", "desc")
+        .onSnapshot((snapshot => {
+          const orderArray = snapshot.docs.map(doc => ({
+            id: doc.id,
+            ...doc.data()
+          }));
+          setOrders(orderArray);
+        }));
+      } else {
+        dbService.collection("orders").where("name", "==", user.email)
+        .orderBy("location", "desc")
+        .onSnapshot((snapshot => {
+          const orderArray = snapshot.docs.map(doc => ({
+            id: doc.id,
+            ...doc.data()
+          }));
+          setOrders(orderArray);
+        }));
+      }
+    }
+  }, [user]);
+
   return (
     <div className="w-75 mx-auto my-5">
-      <div>
-        <h4>2022-07-17</h4>
-        <p>Elly - Gimbpa01 (2EA) AAA Station</p>
-        <p>Mocha - Gimbpa01 (2EA) AAA Station</p>
-        <h4>2022-07-18</h4>
-        <p>Elly - Gimbpa01 (2EA) AAA Station</p>
-        <p>Mocha - Gimbpa01 (2EA) AAA Station</p>
-      </div>
-      <div>
-        <h4>Order01</h4>
-        <p>Gimbpa01 (2EA) AAA Station</p>
-        <h4>Order02</h4>
-        <p>Gimbpa01 (2EA) AAA Station</p>
-        <h4>Order03</h4>
-        <p>Gimbpa01 (2EA) AAA Station</p>
-        <h4>Order04</h4>
-        <p>Gimbpa01 (2EA) AAA Station</p>
-      </div>
+      
     </div>
   );
 }
+
+Orders.propTypes = {
+  user: PropTypes.object
+};
 
 export default Orders;
