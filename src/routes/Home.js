955adfb9270d6d7fca9863e731551f5acab05a0c
Eunji-Elly-Lee <eunji.lee@edu.sait.ca>
2024-06-05 14:58:44 -0600

    Update Home component - enhancing styling

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,24 +1,26 @@
-import { Container, Row, Col } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
-import 'routes/Home.css';
+import { Container, Row, Col } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import "routes/Home.css";
 
 function Home({ user }) {
   return (
-    <div className="home">
+    <div className="home d-flex align-items-center">
       <Container>
         <Row className="justify-content-evenly">
-          <Col xs="5" sm="4" md="3"
-            className="main-menu m-5 m-sm-2 mt-sm-5 position-relative">
-            <Link to="/menu"
-              className="w-100 h-100 d-flex align-items-center text-center">
+          <Col md className="main-menu position-relative bg-black">
+            <Link
+              to="/menu"
+              className="w-100 h-100 d-flex align-items-center text-center"
+            >
               <span className="w-100 text-white fs-2">Menu</span>
             </Link>
           </Col>
           {user && (
-            <Col xs="5" sm="4" md="3"
-              className="main-menu m-5 m-sm-2 mt-sm-5 position-relative">
-              <Link to="/orders"
-                className="w-100 h-100 d-flex align-items-center text-center">
+            <Col md className="main-menu position-relative bg-black">
+              <Link
+                to="/orders"
+                className="w-100 h-100 d-flex align-items-center text-center"
+              >
                 <span className="w-100 text-white fs-2">Orders</span>
               </Link>
             </Col>
