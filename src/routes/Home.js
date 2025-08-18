fcb647b8a3b6709e800d9ec3ec3070cba56fd3e5
Eunji-Elly-Lee <eunji.lee@edu.sait.ca>
2025-08-18 23:55:30 +0900

    feat(Home): add intro message before login

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -4,10 +4,14 @@ import "routes/Home.css";
 
 function Home({ user }) {
   return (
-    <div className="home d-flex align-items-center">
+    <div className={`home ${user ? "home-logged-in" : ""} d-flex align-items-center`}>
       <Container>
-        <Row className="justify-content-evenly">
-          <Col md className="main-menu position-relative bg-black">
+        <Row
+          className={`
+            main-menu-wrapper ${!user ? "wrapper-logged-out" : ""} p-5 justify-content-evenly
+          `}
+        >
+          <Col md="6" className="main-menu position-relative bg-black">
             <Link
               to="/menu"
               className="w-100 h-100 d-flex align-items-center text-center"
@@ -15,14 +19,18 @@ function Home({ user }) {
               <span className="w-100 text-white fs-2">Menu</span>
             </Link>
           </Col>
-          {user && (
-            <Col md className="main-menu position-relative bg-black">
+          {user ? (
+            <Col md="6" className="main-menu position-relative bg-black">
               <Link
                 to="/orders"
                 className="w-100 h-100 d-flex align-items-center text-center"
               >
                 <span className="w-100 text-white fs-2">Orders</span>
               </Link>
+            </Col>
+          ) : (
+            <Col md="6" className="intro-text my-auto pt-3 pb-2 px-5 shadow rounded">
+              <h1>Pick Up Your Yummy Kimbap!</h1>
             </Col>
           )}
         </Row>
